031f5e525e3d6caabff930920927ed732a181a2a
gmulier <guillaume.mulier@icloud.com>
2023-09-20 11:24:27 +0200

    update terrain

--- terrain.py
+++ terrain.py
@@ -1,3 +1,3 @@
 class Terrain(object):
     def __init__(self):
-        self.tableau = [[1 for i in range(14)] for j in range(20)]
+        self.tableau = [[0 if i in [0, 13] else 1 for i in range(14)] for j in range(20)]
