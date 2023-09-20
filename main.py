004cc33b4b55e8e2be996224b1f893ffd1dbf2a5
gmulier <guillaume.mulier@icloud.com>
2023-09-20 10:22:34 +0200

    Ajout du framerate

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import pygame as pg
 
+FRAMERATE = 60 # Set fps
+
 def main():
     pg.init()
     windowSize = (1000, 800)
     screen = pg.display.set_mode(windowSize)
     GameOver = False
+    
+    clockFramerate = pg.time.Clock()
+
     while not GameOver:
         for event in pg.event.get():
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_ESCAPE:
                     GameOver = True
+        
+        clockFramerate.tick(FRAMERATE)
+
     pg.quit()
 
 
