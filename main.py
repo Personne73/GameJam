cdfd68a1a4147bd599f64b24c8a8e1ef543438b0
AurelFi <aurelien.fiche.esipe@gmail.com>
2023-09-20 15:48:25 +0200

    Poulet fonctionnel avec animations

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame as pg
 import sys
 
 from land.terrain import Terrain
+from player import Personnage
 
 FRAMERATE = 60 # Set fps
 CASE_SIZE = 40 #pixels
@@ -15,8 +16,9 @@ def main():
     screen = pg.display.set_mode(WINDOW_SIZE)
     game_over = False
     terrain = Terrain()
-
+    
     clock_framerate = pg.time.Clock()
+    player = Personnage()
     while not game_over:
         terrain.update(screen)
         for event in pg.event.get():
@@ -26,12 +28,21 @@ def main():
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_ESCAPE:
                     game_over = True
-                if event.key == pg.K_z:
+                elif event.key == pg.K_z:
+                    player.move(0, 1)
                     terrain.shift_terrain(screen)
+                elif event.key == pg.K_s:
+                    player.move(0, -1)
+                elif event.key == pg.K_d:
+                    player.move(1, 0)
+                elif event.key == pg.K_q:
+                    player.move(-1, 0)
 
-        clock_framerate.tick(FRAMERATE)
+        # screen.fill("#579928")
+        screen.fill("#b1d670")
+        player.draw(screen)
         pg.display.flip()
-
+        clock_framerate.tick(FRAMERATE)
     pg.quit()
 
 
--- player.py
+++ player.py
@@ -1,21 +1,67 @@
 import pygame as pg
-from main import *
 
-class Personnage:
-    def __init__(self, image_path):
-        self.image = pg.image.load(image_path)
-        self.position = [6, 19]
-        self.rect = self.image.get_rect()
-        self.rect.x = self.position[0] * CASE_SIZE  # Position initiale en pixels (6ème colonne)
-        self.rect.y = self.position[1] * CASE_SIZE  # Position initiale en pixels (dernière ligne)
+class Personnage(object):
+    MOVE_STEPS = 8
+    buffer_x, buffer_y = 0, 0
+    pos_x, pos_y = 240, 520
+    jmp = 0
+    angle = 0
 
-    def draw(self, window):
-        window.blit(self.image, self.rect)
+    def __init__(self):
+        self.image = pg.image.load("chicken.png")
+        self.image = pg.transform.scale(self.image, (40, 40))
 
-    def move(self, x, y):
-        # Vérifier si le déplacement est valide
-        new_x = self.rect.x + x
-        new_y = self.rect.y + y
-        if 0 <= new_x < WINDOW_SIZE[0] - self.rect.width and 0 <= new_y < WINDOW_SIZE[1] - self.rect.height:
-            self.rect.x = new_x
-            self.rect.y = new_y
+    def draw(self, screen):
+        img = self.image
+        offset = 0
+        # X animation
+        if (self.buffer_x < 0):
+            self.pos_x -= 40 / self.MOVE_STEPS
+            self.buffer_x += 1
+        elif (self.buffer_x > 0):
+            self.pos_x += 40 / self.MOVE_STEPS
+            self.buffer_x -= 1
+        # Y animation
+        if (self.buffer_y < 0):
+            self.pos_y -= 40 / self.MOVE_STEPS
+            self.buffer_y += 1
+        elif (self.buffer_y > 0):
+            self.pos_y += 40 / self.MOVE_STEPS
+            self.buffer_y -= 1
+        # Jump animation
+        if (self.jmp != 0):
+            factor = 1 + (self.MOVE_STEPS - abs(self.jmp - self.MOVE_STEPS)) / (self.MOVE_STEPS * 10)
+            img = pg.transform.scale(self.image, (40 * factor, 40 * factor))
+            offset = (40 * factor - 40) / 2
+            self.jmp += 1
+        if (self.jmp == 2 * self.MOVE_STEPS):
+            self.jmp = 0
+        screen.blit(img, (self.pos_x - offset, self.pos_y - offset))
+
+    def get_pos(self):
+        '''
+        Retourne la position du joueur en nombre de cases
+        '''
+        return (int(self.pos_x/40), int(self.pos_y/40))
+    
+    def pos_to_angle(self, dx, dy):
+        if (dy == 0):
+            return -90 if dx > 0 else 90
+        else:
+            return 0 if dy > 0 else 180
+
+    def move(self, dx: int, dy: int, blocked: bool = False):
+        '''
+        dx : Nombre de cases sur l'axe x [-1, 1]
+        dy : Nombre de cases sur l'axe y [-1, 1]
+        blocked : Si True, le joueur ne sera pas déplacé
+        '''
+        if (not blocked):
+            self.buffer_x += dx * self.MOVE_STEPS
+            self.buffer_y -= dy * self.MOVE_STEPS
+        new_angle = self.pos_to_angle(dx, dy)
+        if (self.angle != new_angle):
+            self.image = pg.transform.rotate(self.image, - self.angle + new_angle)
+            self.angle = new_angle
+        if (self.jmp == 0):
+            self.jmp = 1
