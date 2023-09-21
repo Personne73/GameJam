d41814a3b658d77c9b83845556ee1b802b7b84cd
gmulier <guillaume.mulier@icloud.com>
2023-09-21 14:56:10 +0200

    Generation du terrain et defilement

--- land/terrain.py
+++ land/terrain.py
@@ -3,61 +3,13 @@ import pygame as pg
 from enum import Enum
 import os
 
-
-
-class Obstacle(pg.sprite.Sprite):
-    def __init__(self, x, y, obstacle_image):
-        super(Obstacle, self).__init__()
-        self.x = x
-        self.y = y
-
-        # Load the image, preserve alpha channel for transparency
-        self.surf = pg.image.load(obstacle_image).convert_alpha()
-
-        # Save the rect so you can move it
-        self.rect = self.surf.get_rect()
-
-    def update(self, screen, pos):
-        '''
-        pos : Tuple
-        '''
-        self.rect = pos
-
-        # draw the image in the screen
-        screen.blit(self.surf, self.rect)
-
-class Bush(Obstacle):
-    def __init__(self, x, y):
-        self.bush = os.path.abspath("./images/bush.png")
-        super().__init__(x, y, self.bush)
-
-class Rock(Obstacle):
-    def __init__(self, x, y):
-        self.rock_image = os.path.abspath("./images/rock.png")
-        super().__init__(x, y, self.rock_image)
-
-class TreeTrunk(Obstacle):
-    def __init__(self, x, y):
-        self.tree_trunk_image = os.path.abspath("./images/trunk.png")
-        super().__init__(x, y, self.tree_trunk_image)
-
-class Tree(Obstacle):
-    def __init__(self, x, y):
-        self.tree_image = os.path.abspath("./images/tree.png")
-        super().__init__(x, y, self.tree_image)
-
-class Road():
-    def __init__(self) -> None:
-        self.sens
-
 class TypeTerrain(Enum):
     GRASS = 1
-    #WATER = 2
     ROAD = 2
     GLITCH = 3
 
 class Cell():
-    def __init__(self, type_terrain, obstacle=False):
+    def __init__(self, type_terrain, obstacle=0):
         self.type_terrain = type_terrain
         self.obstacle = obstacle
 
@@ -67,19 +19,25 @@ class Terrain(object):
     def __init__(self):
         self.tableau = [[]for j in range(20)]
         self.init_terrain()
-        for i in range(20):
-            for j in range(1, 13):
-                print("Type :", self.tableau[i][j].type_terrain, "Obstacle :", self.tableau[i][j].obstacle)
-        #self.init_terrain()
+
+        self.image_bush = pg.image.load(os.path.abspath("./images/bush.png")).convert_alpha()
+        self.image_rock = pg.image.load(os.path.abspath("./images/rock.png")).convert_alpha()
+        self.image_trunk = pg.image.load(os.path.abspath("./images/trunk.png")).convert_alpha()
+        self.image_tree = pg.image.load(os.path.abspath("./images/tree.png")).convert_alpha()
+
+        self.images_obstacles = {
+            1: self.image_bush,  # Vers le haut (direction y négative)
+            2: self.image_rock,  # Vers le bas (direction y positive)
+            3: self.image_trunk,  # Vers la gauche (direction x négative)
+            4: self.image_tree,  # Vers la droite (direction x positive)
+        }
+
 
     def add_random_obstacles(self, line):
-        obstacles = [Tree(0, 0), Bush(0,0), Rock(0,0), TreeTrunk(0, 0)]
         if self.tableau[line][1].type_terrain == TypeTerrain.GRASS:
             for x in range(1, 13):
                 if random.randint(1,10) in range(8): #permet de tirer 70% du temps un obstacle
-                    self.tableau[line][x].obstacle = random.choice(obstacles)
-                    self.tableau[line][x].obstacle.x = x * 40
-                    self.tableau[line][x].obstacle.y = line * 40
+                    self.tableau[line][x].obstacle = random.randint(1, 4)
 
         
     def create_random_line(self):
@@ -92,29 +50,31 @@ class Terrain(object):
             self.add_random_obstacles(y)
     
 
-    # def shift_terrain(self):
-    #     for
+    def shift_terrain(self,screen):
+        for y in range(18, -1, -1):
+            for x in range(1, 13):
+                self.tableau[y + 1][x] = self.tableau[y][x]
+
+        # for i in range(20):
+        #     print("___________________________________________________________________")
+        #     for j in range(1, 13):
+        #         print("Type :", self.tableau[i][j].type_terrain, "Obstacle :", self.tableau[i][j].obstacle)
     
-        #     from obstacle.tree import Tree
-        # from obstacle.bush import Bush
-        # from obstacle.rock import Rock
-        # from obstacle.tree_trunk import TreeTrunk
-
-        # obstacle_list = [0 if i in [0, 13] else None for i in range(14)]
-        # obstacles = [Tree(0, 0), Bush(0,0), Rock(0,0), TreeTrunk(0, 0)]
-        # for i in range(1, 13):
-        #     if random.randint(0, 4) in [0, 1, 2]:
-        #         obstacle_list[i] = random.choice(obstacles)
-        #         obstacle_list[i].x = i * 40
-        #         obstacle_list[i].y = y * 40
-        # return obstacle_list
-
     def draw_grass(self, screen, y):
         pg.draw.rect(screen, (117, 214, 112), (40, y * 40, 12 * 40, 40))
-        
+
         for x in range(1, 13):
-            if self.tableau[y][x].obstacle != False:
-                self.tableau[y][x].obstacle.update(screen, (self.tableau[y][x].obstacle.x, self.tableau[y][x].obstacle.y))
+            if self.tableau[y][x].obstacle:
+                # Récupérez l'image d'obstacle correspondante à partir du dictionnaire
+                obstacle_image = self.images_obstacles.get(self.tableau[y][x].obstacle, None)
+                
+                if obstacle_image:
+                    # Dessinez l'image d'obstacle sur l'écran
+                    screen.blit(obstacle_image, (x * 40, y * 40))
+        
+        # for x in range(1, 13):
+        #     if self.tableau[y][x].obstacle:
+        #         self.tableau[y][x].obstacle.update(screen, (self.tableau[y][x].obstacle.x, self.tableau[y][x].obstacle.y))
 
     def draw_road(self, screen, y):
         pg.draw.rect(screen, (100, 100, 100), (40, y * 40, 12 * 40, 40))
@@ -126,6 +86,6 @@ class Terrain(object):
                 self.draw_grass(screen, y)    
             elif self.tableau[y][1].type_terrain == TypeTerrain.ROAD:
                 self.draw_road(screen, y)
+        
 
-        #print("\n")
             
--- main.py
+++ main.py
@@ -11,26 +11,10 @@ def main():
     window_size = (560, 800)
     screen = pg.display.set_mode(window_size)
     game_over = False
-
-    #tree = Tree(0, 0)
-    #bush = Bush(window_size[0] / 2, window_size[1] / 2)
-    #rock = Rock(100, 100)
-    #trunk = TreeTrunk(200, 200)
-    #void = Void(window_size[0] / 2, window_size[1] / 2)
-    #grass = Row("grass")
     terrain = Terrain()
 
     clock_framerate = pg.time.Clock()
     while not game_over:
-        # draw a rect in the middle of the screen
-        # pg.draw.rect(screen, (255, 0, 0), (window_size[0] / 2 - 50, window_size[1] / 2 - 50, 100, 100))
-        # screen.blit(tree_image, (tree.x, tree.y))
-        # grass.update(screen, 0)
-        # bush.update(screen, (bush.x, bush.y))
-        # tree.update(screen, (tree.x, tree.y))
-        # rock.update(screen, (rock.x, rock.y))
-        # trunk.update(screen, (trunk.x, trunk.y))
-        # void.update(screen, (void.x, void.y))
         terrain.update(screen)
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -39,6 +23,8 @@ def main():
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_ESCAPE:
                     game_over = True
+                if event.key == pg.K_z:
+                    terrain.shift_terrain(screen)
 
         clock_framerate.tick(FRAMERATE)
         pg.display.flip()
